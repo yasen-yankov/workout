1e9296fe4340a930efb2d72507de7dff4f639a29
Yasen Yankov <yasen.yankov@telerik.com>
2014-10-14 15:52:05 +0000

    Changes made by Yasen Yankov (yasen.yankov@telerik.com).

--- Workout/scripts/app/app.js
+++ Workout/scripts/app/app.js
@@ -1,5 +1,22 @@
 var app = (function (window) {
+    var mobileApp,
+        devicePlatform,
+        mobileAppSettings = {
+            transition: 'fade',
+            skin: 'flat',
+            initial: 'views/startWorkout.html'
+        };
+
     document.addEventListener('deviceready', function () {
+        devicePlatform = device.platform;
+
+        if (devicePlatform == "Android") {
+            mobileAppSettings.useNativeScrolling = true;
+            mobileAppSettings.transition = "none";
+        }
+
+        app.mobileApp = new kendo.mobile.Application(document.body, mobileAppSettings);
+
         navigator.splashscreen.hide();
     }, false);
 
@@ -7,21 +24,6 @@ var app = (function (window) {
         apiKey: appSettings.everlive.apiKey,
         scheme: appSettings.everlive.scheme
     });
-    
-    var mobileAppSettings = {
-        transition: 'fade',
-        skin: 'flat',
-        initial: 'views/startWorkout.html'
-    };
-    
-    var devicePlatform = device.platform;
-    
-    if (devicePlatform == "Android") {
-        mobileAppSettings.useNativeScrolling = true;
-        mobileAppSettings.transition = "none";
-    }
-    
-    var mobileApp = new kendo.mobile.Application(document.body, mobileAppSettings);
 
     return {
         mobileApp: mobileApp,
